b0ac029886d74268e9572b86cefcf09621d8a867
Menlazar <ivan@menlazar.de>
2022-02-06 19:03:52 -0600

    new

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+import os
+
+client = discord.Client()
+huntTime = int(60)
+eventValue = float(0.25)
+donatorRole = float(0.35)
+donorHunt = huntTime * donatorRole
+eventHunt = huntTime - donorHunt * eventValue
+totalTime = huntTime - donorHunt - eventHunt
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$test'):
+        await message.channel.send('Hello!')
+    if message.content.startswith('$time'):
+        await message.channel.send('tiempo restante en hunt es: ' + str(totalTime) )
+
+client.run('OTM5OTk0MzAzOTY1NTkzNjAw.YgA76g.Aj5CGftmvLaLJXGnyO-KaLqueRs')
