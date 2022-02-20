82eca134f59367818ca103468a34cb975b80d644
Menlazar <ivan@menlazar.de>
2022-02-19 23:53:36 -0600

    env file

--- main.py
+++ main.py
@@ -16,7 +16,12 @@ totalTime = donatorhunt - eventTime
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-#again xD
+
+@client.command()
+async def italic(ctx):
+    response = 'This text has some words *emphasized* in _different_ ways'
+    await ctx.send(response)
+
 @client.event
 async def on_message(message):
     dcuser = message.author.name
